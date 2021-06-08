12dd0e6a5ddf455cc94eed71c23bf5376fd04994
Steven Diamante <StevenDiamante13@gmail.com>
2021-06-08 12:43:14 -0500

    Home page - renders heading, add new tasks when enter is clicked, clears input field after pressing enter.

--- pages/__tests__/index.test.js
+++ pages/__tests__/index.test.js
@@ -1,11 +1,60 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
-import { render, screen } from "@testing-library/react";
 import Home from "../index";
 
 describe("<Home />", () => {
-  it("renders Hello", () => {
-    render(<Home />);
+  it("renders the text Task Manager", () => {
+    renderComponent();
 
-    expect(screen.getByText("Hello")).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /Task Manager/i })
+    ).toBeInTheDocument();
+  });
+
+  it("adds new tasks when enter is clicked", async () => {
+    renderComponent();
+
+    addNewTask("Take out the trash");
+    addNewTask("Write Blog Post");
+
+    const listItems = await screen.findAllByRole("listitem");
+    expect(listItems.length).toBe(2);
+    expect(listItems[0].textContent).toBe("Take out the trash");
+    expect(listItems[1].textContent).toBe("Write Blog Post");
+  });
+
+  it("clears input field after pressing enter", () => {
+    renderComponent();
+
+    type(getTaskInputField(), "Clear this after pressing enter, please");
+    expect(getTaskInputField().value).toBe(
+      "Clear this after pressing enter, please"
+    );
+    
+    pressEnter(getTaskInputField());
+    expect(getTaskInputField().value).toBe("");
   });
 });
+
+const renderComponent = () => {
+  render(<Home />);
+};
+
+const addNewTask = (taskName) => {
+  const taskInputField = getTaskInputField();
+  type(taskInputField, taskName);
+  pressEnter(taskInputField);
+};
+
+const getTaskInputField = () => {
+  return screen.getByRole("textbox", { name: /Add New Task/i });
+};
+
+const type = (input, text) => {
+  fireEvent.change(input, { target: { value: text } });
+};
+
+const pressEnter = (domElement) => {
+  fireEvent.keyPress(domElement, { key: "Enter", keyCode: 13 });
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,62 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from 'react';
+import uuid from 'uuid-random';
+import { Task, Tasks } from "../interfaces/interfaces"
 
 const Home = () => {
+  const initialState = {
+    id: "",
+    name: "",
+    completed: false
+  }
+
+  const UUID = uuid();
+
+  const [task, setTask] = useState<Task>(initialState);
+  const [tasks, setTasks] = useState<Tasks>([]);
+
+  useEffect(() => {
+    console.log(`home component render - CURRENT TASKS: ${JSON.stringify(tasks)}`)
+  }, [tasks])
+
+  const handleChangeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask({
+      id: UUID,
+      name: event.target.value,
+      completed: false
+    });
+  }
+
+  const handleKeyEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && task.name !== "") {
+      console.log(`***Enter pressed*** current task ${task.name}`)
+      setTasks([...tasks, task])
+      setTask(initialState);
+    }
+  }
+
+  const listItems = tasks.map((task) =>
+    <li key={task.id}>{task.name}</li>
+  );
+
   return (
     <div>
-      Hello
+      <h1>Task Manager</h1>
+
+      <div>
+        <label htmlFor="task">Add New Task</label>
+        <input
+          id="task"
+          name="task"
+          type="text"
+          value={task.name}
+          onChange={handleChangeEvent}
+          onKeyPress={handleKeyEvent} />
+      </div>
+      <div>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
     </div>
   )
 };
